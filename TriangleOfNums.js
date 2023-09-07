975a5dfbefe5b89f52b4d190c4d088bfb032bfb5
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-09-07 15:08:37 +0300

    TriangleOfNums.js

---
+++ TriangleOfNums.js
@@ -0,0 +1,12 @@
+function solve(num) {
+    for (let i = 1; i <= num; i ++) {
+        let line = '';
+        for (let j = 1; j <= i; j ++) {
+        line += i;
+        if (j !== i) {
+            line += " ";
+        } 
+    }
+    console.log(line);
+    }
+}

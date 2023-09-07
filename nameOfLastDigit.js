96b5281a20e794f2a35a1c844b2922dcffa5d517
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-09-07 19:30:59 +0300

    Create nameOfLastDigit.js

---
+++ nameOfLastDigit.js
@@ -0,0 +1,31 @@
+function solve(num) {
+    let number = num;
+    let lastone = +number.toString().split("").pop();
+    switch (lastone) {
+        case lastone: if (lastone === 1) {
+            console.log(`one`); break;
+        } else if (lastone === 2) {
+            console.log(`two`); break;
+        } else if (lastone === 3) {
+            console.log(`three`); break;
+        } else if (lastone === 4) {
+            console.log(`four`); break;
+        } else if (lastone === 5) {
+            console.log(`five`); break;
+        } else if (lastone === 6) {
+            console.log(`six`); break;
+        } else if (lastone === 7) {
+            console.log(`seven`); break;
+        } else if (lastone === 8) {
+            console.log(`eight`); break;
+        } else if (lastone === 9) {
+            console.log(`nine`); break;
+        } else if (lastone === 10) {
+            console.log(`ten`); break;
+        } else if (lastone === 0) {
+            console.log(`zero`); break;
+        }
+
+    }
+
+}

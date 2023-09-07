9482c58aadea40092bc85c63495eeffa3ebe4ab4
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-09-07 15:03:14 +0300

    Update

--- sumOfOdd.js
+++ sumOfOdd.js
@@ -2,4 +2,4 @@ function oddNumsAndSum(N) {
     return Array.from({ length: N * 2 }, (i, n) => n + 1).filter(el => el % 2 != 0).join('\n') + `\nSum: ${N * N}`;
 }
 
-/ This code generates a list of odd numbers from 1 to 2 times "N," formats them as a string with each number on a new line, and appends the sum of "N" squared to the end of the string. /
+/* This code generates a list of odd numbers from 1 to 2 times "N," formats them as a string with each number on a new line, and appends the sum of "N" squared to the end of the string. */

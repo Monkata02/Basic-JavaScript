043fde045cb3ff72a7655aacb5a7c3b574d755f3
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-09-08 14:19:00 +0300

    Create listingNames.js

---
+++ listingNames.js
@@ -0,0 +1,9 @@
+function solve(arr) {
+
+    arr.sort((a,b) => a.localeCompare(b));
+
+    for (let index = 0; index < arr.length; index++) {
+        console.log(`${index + 1}.${arr[index]}`);
+    }
+
+}

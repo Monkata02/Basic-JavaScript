ae852e0e58c540c2f11276d4e0e3d23a06554a34
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-09-08 14:20:25 +0300

    Create arrayTwoCriteria.js

---
+++ arrayTwoCriteria.js
@@ -0,0 +1,11 @@
+function solve(arr) {
+
+    arr.sort(twoCriteriaSort);
+    return arr.join('\n');
+    function twoCriteriaSort(current, next) {
+        if(current.length === next.length) {
+            return current.localeCompare(next);
+        } 
+        return current.length - next.length;
+    }
+}

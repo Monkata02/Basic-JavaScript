37f39fc8d20c4f987d93ee6c9a7e50ea64fdef01
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-09-10 16:52:26 +0300

    Create subtraction.js

---
+++ subtraction.js
@@ -0,0 +1,8 @@
+function subtract() {
+    let num1 = document.getElementById('firstNumber').value;
+    let num2 = document.getElementById('secondNumber').value;
+    let result = document.getElementById('result');
+
+    result.textContent = Number(num1 - num2);
+
+}

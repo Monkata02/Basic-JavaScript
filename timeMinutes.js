e0b85870a240e1abaeaaea4a11682cdf1c29053c
Simeon <144338083+Monkata02@users.noreply.github.com>
2023-10-01 19:13:44 +0300

    Create timeMinutes.js

---
+++ timeMinutes.js
@@ -0,0 +1,24 @@
+function timeMinutes(input) {
+
+    let hours = Number(input[0]);
+    let minutes = Number(input[1]) + 15;
+
+
+    if (minutes > 59) {
+        hours = hours + 1;
+        minutes = minutes - 60;
+    }
+
+    if (hours > 23) {
+        hours = hours - 24;
+    }
+
+    if (minutes < 10) {
+        console.log(`${hours}:0${minutes}`);
+    } else {
+        console.log(`${hours}:${minutes}`);
+    }
+
+
+}
+timeMinutes(["23", "46"]);
